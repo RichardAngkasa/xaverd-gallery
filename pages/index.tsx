67a72e98c92f56d3b10446350c0771364d768e5f
RichardAngkasa <richardangkasa01@gmail.com>
2023-04-22 23:05:19 +0700

    first commit

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,124 +1,239 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+/* eslint-disable @next/next/no-img-element */
 
-const inter = Inter({ subsets: ['latin'] })
+import { useState } from "react";
+import data from "../public/src.json";
+import close from "../public/images/close.svg";
+import { Raleway } from "next/font/google";
+
+const rale = Raleway({
+  weight: ["100", "200", "300", "400", "500"],
+  subsets: ["latin"],
+});
+
+interface Detail {
+  id: number;
+  setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+}
+
+interface Nav {
+  setNav: React.Dispatch<React.SetStateAction<boolean>>;
+}
 
 export default function Home() {
+  const [open, setOpen] = useState(false);
+  const [nav, setNav] = useState(false);
+  const [id, setId] = useState(0);
+
+  const handleOpen = (id: number) => {
+    setOpen(true);
+    setId(id);
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
+    <div>
+      {nav ? (
+        <>
+          <Nav setNav={setNav} />
+        </>
+      ) : (
+        ""
+      )}
+      {open ? (
+        <>
+          <Details id={id} setOpen={setOpen} />
+        </>
+      ) : (
+        ""
+      )}
+      <div className={`${open ? "fixed w-screen" : "relative"}`}>
+        <div className={`${rale.className}  container px-4 mx-auto`}>
+          {/* Header */}
+          <div className="">
+            <div className="py-7 mx-auto flex justify-between items-center">
+              <span className="text-2xl font-normal">XAVERD GALLERY</span>
+              <div className="font-light text-base gap-4 flex">
+                <button
+                  onClick={() => {
+                    setNav(true);
+                  }}
+                >
+                  CONTACT
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="w-full h-[1px] bg-black/20"></div>
+          <div>
+            <img
+              src={data[0].image}
+              alt=""
+              className="w-full max-h-[40rem] my-7 object-cover cursor-pointer"
+              onClick={() => {
+                handleOpen(0);
+              }}
             />
-          </a>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2  my-10 md:my-20">
+            <img
+              src={data[6].image}
+              alt=""
+              className="col-span-1 px-0 md:px-10 xl:px-24 w-full cursor-pointer"
+              onClick={() => {
+                handleOpen(6);
+              }}
+            />
+            <div className="flex justify-center  space-y-4 py-4 md:py-0 flex-col md:px-10 ">
+              <div onClick={() => handleOpen(6)} className="cursor-pointer">
+                <h1 className="text-3xl md:text-4xl font-extralight ">
+                  {data[6].title}
+                </h1>
+                <h1 className="text-xl md:text-2xl font-thin">
+                  {data[6].date}
+                </h1>
+              </div>
+              <h1
+                onClick={() => handleOpen(6)}
+                className="font-extralight text-lg md:font-xl cursor-pointer"
+              >
+                {data[6].description}
+              </h1>
+            </div>
+          </div>
+          <div className="mt-10 md:mt-20">
+            <div className="text-3xl font-extralight mb-8 md:mb-10">
+              last seasson
+            </div>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-5">
+              <div>
+                <img
+                  src={data[1].image}
+                  alt=""
+                  className="max-h-[50rem] w-full object-cover"
+                  onClick={() => {
+                    handleOpen(1);
+                  }}
+                />
+                <div className="my-2 md:my-4 space-y-1 md:space-y-2">
+                  <div className="text-xl md:text-2xl font-extralight">
+                    {data[1].title}
+                  </div>
+                  <div className="text-lg md:text-xl font-thin">
+                    {data[1].date}
+                  </div>
+                </div>
+              </div>
+              <div>
+                <img
+                  src={data[13].image}
+                  alt=""
+                  className="max-h-[50rem] w-full object-cover"
+                  onClick={() => {
+                    handleOpen(13);
+                  }}
+                />
+                <div className="my-2 md:my-4 space-y-1 md:space-y-2">
+                  <div className="text-xl md:text-2xl font-extralight">
+                    {data[13].title}
+                  </div>
+                  <div className="text-lg md:text-xl font-thin">
+                    {data[13].date}
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* <div className={`overflow-hidden`}>
+            <div className="h-fit grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-10 ">
+              {data.slice(0, 8).map((unit, index) => {
+                return (
+                  <>
+                    <img
+                      src={unit.image}
+                      alt={unit.title}
+                      className="aspect-square object-cover w-full cursor-pointer duration-200"
+                      onClick={() => {
+                        handleOpen(index);
+                      }}
+                    />
+                  </>
+                );
+              })}
+            </div>
+          </div> */}
+
+          {/* footer */}
+          <div>
+            <div className="w-full h-[1px] bg-black/20 mt-10"></div>
+            <div className="my-10 flex flex-col space-y-4 md:space-y-0 md:flex-row justify-between">
+              <div>© 2023 | XAVERD GALLERY</div>
+              <div className="flex flex-col md:flex-row space-y-1 md:space-y-0 md:space-x-3 font-light">
+                <a
+                  href="https://www.instagram.com/xaverdred/?hl=id"
+                  target="_blank"
+                >
+                  Instagram
+                </a>
+                <div>Twitter</div>
+                <div>Email</div>
+              </div>
+            </div>
+          </div>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`${inter.className} mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p
-            className={`${inter.className} m-0 max-w-[30ch] text-sm opacity-50`}
-          >
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
+
+const Details = ({ id, setOpen }: Detail) => {
+  return (
+    <div className={rale.className}>
+      <div className="h-screen flex mx-auto fixed md:px-10 w-screen backdrop-blur-lg bg-white/90 z-10 overflow-scroll">
+        <div className="mx-auto container px-4 pb-10 pt-7 h-fit">
+          <div className="flex justify-end pb-4">
+            <button onClick={() => setOpen(false)}>
+              <img src={close.src} alt="" className="h-6 w-6"></img>
+            </button>
+          </div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
+            <img src={data[id].image} alt="" />
+            <div className="col-span-1 space-y-4 mx-0 md:mx-8 ">
+              <div className="space-y-1">
+                <h1 className="text-2xl md:text-3xl font-light">
+                  {data[id].title}
+                </h1>
+                <h1 className="text-base md:text-lg font-light">
+                  {data[id].date}
+                </h1>
+              </div>
+              <div>{data[id].description}</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+const Nav = ({ setNav }: Nav) => {
+  return (
+    <div className="h-screen flex flex-col fixed md:px-10 w-screen backdrop-blur-lg bg-white/90 z-10">
+      <div className="flex justify-end w-full mt-7 px-4">
+        <button onClick={() => setNav(false)}>
+          <img src={close.src} alt="" className="h-6 w-6"></img>
+        </button>
+      </div>
+      <div
+        className={`${rale.className} w-1/2 h-2/3 px-4 flex justify-start  space-y-4 font-light mx-auto`}
+      >
+        <div className="flex flex-col justify-center items-center mx-auto font-light text-2xl space-y-4">
+          <a href="https://www.instagram.com/xaverdred/?hl=id" target="_blank">
+            Instagram
+          </a>
+          <div>Twitter</div>
+          <div>Email</div>
+        </div>
+      </div>
+    </div>
+  );
+};
